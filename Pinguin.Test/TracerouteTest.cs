56ce38fd27553ab5384055eaef3c156db315bdc4
kindaconfusion <kindaconfusion@gmail.com>
2024-12-30 16:08:40 -0600

    add traceroute

---
+++ Pinguin.Test/TracerouteTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using System.Net;
+using Pinguin.Models;
+
+namespace Pinguin.Test;
+
+[TestFixture]
+public class Tests
+{
+
+    [Test]
+    public void Test()
+    {
+        Console.WriteLine("huh");
+        Assert.Pass();
+    }
+    
+    [Test]
+    public async Task Test1()
+    {
+        var SHIT = new PingObject(IPAddress.Parse("8.8.8.8"));
+        var trace = await Traceroute.RunTraceroute(SHIT);
+        TestContext.Out.WriteLine(String.Join(", ", trace.Select(x => x.IpAddress)));
+        Assert.Pass();
+    }
+}
---
+++ Pinguin/Models/Traceroute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pinguin.Models;
+
+public static class Traceroute
+{
+    public async static Task<List<PingObject>> RunTraceroute(PingObject host)
+    {
+        var traceroute = new List<PingObject>();
+        var options = new PingOptions();
+        options.Ttl = 1;
+        while (traceroute.Count == 0 || !traceroute.Last().IpAddress.Equals(host.IpAddress))
+        {
+            var cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            var sender = new Ping();
+            var reply = await sender.SendPingAsync(host.IpAddress, new TimeSpan(0, 0, 5), Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), options, token);
+            if (reply.Status == IPStatus.Success || reply.Status == IPStatus.TtlExpired)
+                traceroute.Add(new PingObject(reply.Address));
+            
+            //Console.WriteLine(reply.Address);
+            options.Ttl++;
+        }
+        return traceroute;
+    }
+}
