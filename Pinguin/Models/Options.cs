822818348c554e3a34c0c566bb6a9c4e7a52efea
Justin Petry <justincpetry@gmail.com>
2025-01-08 18:48:41 -0600

    hosts is no longer a property of options

--- Pinguin/Models/Options.cs
+++ Pinguin/Models/Options.cs
@@ -6,13 +6,11 @@ namespace Pinguin.Models;
 
 public partial class Options : ObservableObject
 {
-    [ObservableProperty] private List<string>? _hostNames;
     [ObservableProperty] private double? _interval;
     [ObservableProperty] private int? _packetSize;
 
-    public Options(List<String> hosts, double interval, int packetSize)
+    public Options(double interval, int packetSize)
     {
-        _hostNames = hosts;
         _interval = interval;
         _packetSize = packetSize;
     }
